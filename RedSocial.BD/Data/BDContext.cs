e5cd537dcaced847c7c41cd469c9ffee2b9333d2
Gastonibanez <81576503+Gastonibanez0@users.noreply.github.com>
2022-07-09 21:22:55 -0300

    Agregar archivos de proyecto.

---
+++ RedSocial.BD/Data/BDContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedSocial.BD.Data.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocial.BD.Data
+{
+    public class BDContext : DbContext
+    {
+
+        public DbSet<Home> Home1  { get; set; }
+        public BDContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
