5b7ce054d1be4747ef690f23b81e4ef6856b690e
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-07-18 18:39:50 -0300

    FormLogin

--- RedSocialDeportiva/Client/Pages/LoginAndRegister/Models/FormInputsLogin.cs
+++ RedSocialDeportiva/Client/Pages/LoginAndRegister/Models/FormInputsLogin.cs
@@ -2,11 +2,11 @@
 {
     public class FormInputsLogin
     {
-        public string Placeholder { get; set; }
-        public string Type { get; set; }
-        public string Name { get; set; }
-        public string ExpReg { get; set; }
-        public string ErrorMessage { get; set; }
+        public string? Placeholder { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? ExpReg { get; set; }
+        public string? ErrorMessage { get; set; }
 
     }
 }
---
+++ RedSocialDeportiva/Client/Pages/LoginAndRegister/Models/FormLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace RedSocialDeportiva.Client.Pages.LoginAndRegister.Models
+{
+    public class FormLogin
+    {
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }    
+    }
+}
