0f7c26bdf20b58f804cf0a1dbab41e267a5e9876
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-07-27 21:57:33 -0300

    .

--- RedSocialDeportiva/Client/Pages/LoginAndRegister/Dto/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RedSocialDeportiva.Client.Pages.LoginAndRegister.Dto
-{
-    public class LoginDto
-    {
-        public string? Email { get; set; }
-
-        public string? Password { get; set; }
-    }
-}
