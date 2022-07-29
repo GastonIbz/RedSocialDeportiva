a04d765193c74eec22e1440b64690bf679d52252
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-07-28 23:03:21 -0300

    Service State Container

--- RedSocialDeportiva/Client/Program.cs
+++ RedSocialDeportiva/Client/Program.cs
@@ -1,6 +1,9 @@
 
 #region Realizo importaciones a nivel global para evitar el using en cada pag
+
 global using RedSocialDeportiva.Shared.DTO_Front.LoginAndRegister; // Voy a poder utilziar todos los DTO de LoginAndRegister dentro del CLiente.
+global using RedSocialDeportiva.Client.Pages.LoginAndRegister; // Accedemos al Store que tiene dicha pag.
+
 #endregion
 
 using RedSocialDeportiva.Client;
@@ -12,5 +15,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<LoginAndRegisterStore>();
 
 await builder.Build().RunAsync();
