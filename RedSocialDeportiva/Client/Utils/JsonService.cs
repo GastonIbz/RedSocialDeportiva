e451ad5e381041834dc5bc31ee94c833fa2178fb
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-07-30 17:43:43 -0300

    Utils console log json

---
+++ RedSocialDeportiva/Client/Utils/JsonService.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json; // Permite convertir una clase en un JSON
+
+namespace RedSocialDeportiva.Client.Utils.JsonService
+{
+    public class JsonService
+    {
+        
+        public JsonService() { }
+
+        public static void ShowConsoleDataInJson<T>(T data)
+        { 
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize<T>(data, options);
+            Console.WriteLine("aaaa" + jsonString);
+        }
+
+    }
+}
