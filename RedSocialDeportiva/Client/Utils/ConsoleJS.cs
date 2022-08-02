b31a916cdec012b898c433f89b58e40ad515c6f5
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-02 00:52:56 -0300

    Incorporacion de JS para Console.log

---
+++ RedSocialDeportiva/Client/Utils/ConsoleJS.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+
+namespace RedSocialDeportiva.Client.Utils.ConsoleJS
+{
+    public class ConsoleJS
+    {
+
+        private IJSRuntime _js { get; }
+        public ConsoleJS(IJSRuntime jS)
+        {
+            this._js = jS ?? throw new ArgumentNullException(nameof(jS));
+        }
+
+
+        public async void log<DateType>(string message, DateType data)
+        {
+            await _js.InvokeVoidAsync("console.log", message, data);
+        }
+
+    }
+}
