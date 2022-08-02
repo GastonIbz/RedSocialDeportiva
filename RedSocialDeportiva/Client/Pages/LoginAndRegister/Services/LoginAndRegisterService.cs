a8f5d9bd112a93e74d2dc64fca314fcbcb08dec8
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-02 00:54:10 -0300

    Service Login and Register

---
+++ RedSocialDeportiva/Client/Pages/LoginAndRegister/Services/LoginAndRegisterService.cs
@@ -0,0 +1,27 @@
+﻿using RedSocialDeportiva.Shared;
+using System.Net.Http.Json;
+
+namespace RedSocialDeportiva.Client.Pages.LoginAndRegister.Services
+{
+    public class LoginAndRegisterService
+    {
+
+        /// Inyectamos e inicializamos en nuestra clase, el servicio http
+        private readonly HttpClient http;
+        private ConsoleJS consoleJS;
+       
+        public LoginAndRegisterService(HttpClient http, ConsoleJS consoleJS)
+        {
+            this.http = http;
+            this.consoleJS = consoleJS;
+        }
+
+
+        public async Task probando()
+        {
+            var result = await this.http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast/PROBANDO");
+            consoleJS.log <WeatherForecast[]>("Handle Login", result);
+            Console.WriteLine(result);
+        }
+    }
+}
