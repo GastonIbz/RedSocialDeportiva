51ef962c6c53cd62959af8d1b8b4734f2ab06d22
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2022-08-08 15:30:08 -0300

    Se declara como clave unica a Id de usuario

--- RedSocial.BD/Data/Entidades/Usuario.cs
+++ RedSocial.BD/Data/Entidades/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace RedSocial.BD.Data.Entidades
 {
+    [Index(nameof(Id), Name = "IdUsuario_UQ", IsUnique = true)]
     public class Usuario
     {
         public int Id { get; set; } 
