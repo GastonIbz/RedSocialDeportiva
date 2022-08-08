d0f1c8b661ba56fe801dc4f2490038b52c6644ce
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2022-08-08 16:23:19 -0300

    cree las etiquetas para cada atributo de la clase Usuario

--- RedSocial.BD/Data/Entidades/Usuario.cs
+++ RedSocial.BD/Data/Entidades/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,39 @@ namespace RedSocial.BD.Data.Entidades
 {
     [Index(nameof(Id), Name = "IdUsuario_UQ", IsUnique = true)]
     public class Usuario
-    {
-        public int Id { get; set; } 
+    { 
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "El nombre de usuario no debe superar los {1} caracteres")]
         public string NameUsuario { get; set; }
+
+        [Required] 
+        [MinLength(8, ErrorMessage = "la contraseña debe tener al menos {1} caracteres")] 
+        [DataType(DataType.Password)]   
         public string password { get; set; } 
 
-        public string Email { get; set; } 
+        [DataType(DataType.Password)]
+        public string COnfirmPassword { get; set; }
 
-        public string Descripcion { get; set; } 
+        
+        [Required]
+        [EmailAddress] 
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracter",MinimumLength=1)]
+        public string Email { get; set; }
+
+        [MaxLength(300, ErrorMessage = "La descripcion no debe superar los {1} caracteres")]
+        public string Descripcion { get; set; }  
+
+        [Required]
+        [Url]
+        public string ImgPerfil { get; set; }  
+
+        [Required]
+        [Url]
+        public string ImgPortada { get; set; }
+
 
     }
 }
