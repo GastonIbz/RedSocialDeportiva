aab35214b8a7328d0012bff402c5f68f697db5a0
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-10 00:57:25 -0300

    User Models Front

---
+++ RedSocialDeportiva/Client/Pages/LoginAndRegister/Models/UserModels.cs
@@ -0,0 +1,12 @@
+﻿namespace RedSocialDeportiva.Client.Pages.LoginAndRegister.Models
+{
+    public class UserModels
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string NameCompleted { get; set; }
+        public string Id { get; set; }
+        public string Token { get; set; }
+    }
+}
