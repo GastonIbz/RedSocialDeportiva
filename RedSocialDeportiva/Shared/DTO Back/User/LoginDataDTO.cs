1348d0203b32f48a23cffb45e6889238e1a3e2be
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-10 20:00:16 -0300

    Login DTO

--- RedSocialDeportiva/Shared/DTO Back/User/LoginDataDTO.cs
+++ RedSocialDeportiva/Shared/DTO Back/User/LoginDataDTO.cs
@@ -7,14 +7,19 @@ using System.Threading.Tasks;
 namespace RedSocialDeportiva.Shared.DTO_Back.User
 {
     // TODO: Poner Etiquetas
-    public class UserDTO
+    public class User
     {
         public string UserName { get; set; }
         public string Email { get; set; }
         public string LastName { get; set; }
         public string NameCompleted { get; set; }
         public string Id { get; set; }
-        public string Token{ get; set; }
+    }
 
+    public class LoginDataDTO
+    {
+        public User User { get; set; }
+        public string Token { get; set; }
+        public string MessageError { get; set; }
     }
 }
