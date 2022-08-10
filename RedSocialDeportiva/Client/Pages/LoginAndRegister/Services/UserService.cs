ac41d120badd7909289eb0cbc171a10d257b531e
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-10 00:57:11 -0300

    User Service

---
+++ RedSocialDeportiva/Client/Pages/LoginAndRegister/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using RedSocialDeportiva.Client.Pages.LoginAndRegister.Adapters;
+using RedSocialDeportiva.Shared;
+using System.Net.Http.Json;
+
+namespace RedSocialDeportiva.Client.Pages.LoginAndRegister.Services
+{
+    public class UserService
+    {
+
+        /// Inyectamos e inicializamos en nuestra clase, el servicio http
+        private readonly HttpClient http;
+        private ConsoleJS consoleJS;
+        private LoginAndRegisterAdapter adapter;
+
+        public UserService(HttpClient http, ConsoleJS consoleJS)
+        {
+            this.http = http;
+            this.consoleJS = consoleJS;
+        }
+
+
+        public async Task login()
+        {
+            // TODO: Hacer que envie metodo POST y adaptarlo al respecto,.
+            // TODO: Adaptar a iniciarSesion de LoginAndRegister,, axios Method , Logn.ts (service),
+
+            var result = await this.http.GetFromJsonAsync<UserDTO>("api/User");
+            UserModels UserAdapted = adapter.CreateAdapterUser(result);
+
+
+            consoleJS.log("Handle probando", result);
+
+
+
+        }
+    }
+}
