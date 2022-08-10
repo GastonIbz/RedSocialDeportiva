7810bf0bcc58c79f4212f026301fbfeaa68552e3
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-10 00:57:42 -0300

    Adapters Data User Front

---
+++ RedSocialDeportiva/Client/Pages/LoginAndRegister/Adapters/LoginAndRegisterAdapter.cs
@@ -0,0 +1,22 @@
+﻿namespace RedSocialDeportiva.Client.Pages.LoginAndRegister.Adapters
+{
+    public class LoginAndRegisterAdapter
+    {
+
+        public UserModels CreateAdapterUser(UserDTO dataUser)
+        {
+
+            UserModels formattedUser = new UserModels
+            {
+                Email = dataUser.Email,
+                Id = dataUser.Id,
+                LastName = dataUser.LastName,
+                NameCompleted = dataUser.NameCompleted,
+                UserName = dataUser.UserName,
+                Token = dataUser.Token,
+            };
+
+            return formattedUser;
+        }
+    }
+}
