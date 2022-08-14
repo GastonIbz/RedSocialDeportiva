463a05b0e7e52f04fb38fb3259130f5a8cca9532
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2022-08-14 18:59:05 -0300

    creo carpeta Auth y dentro de ella la clase AuthenticationStateProvider

---
+++ RedSocialDeportiva/Client/Auth/AuthStateProviderFalso.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace RedSocialDeportiva.Client.Auth
+{
+    public class c : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()//Este metodo determina si el usuario
+                                                 //Esta autenticado o no 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
