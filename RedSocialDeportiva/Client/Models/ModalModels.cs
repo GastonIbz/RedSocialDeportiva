10426e812a776d257d07292534a9781c1baf6a05
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-28 20:06:55 -0300

    Modelo de Modals

---
+++ RedSocialDeportiva/Client/Models/ModalModels.cs
@@ -0,0 +1,10 @@
+﻿namespace RedSocialDeportiva.Client.Models
+{
+    public class ModalModels
+    {
+        public bool ShowModal { get; set; }
+        public string? Message { get; set; }
+        public string CssModal { get; set; }
+
+    }
+}
