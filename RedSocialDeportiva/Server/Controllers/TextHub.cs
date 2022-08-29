d9fdcd40068d40465d62307963b4cb8783684e6b
Hoffman Erwin <erwinhoffman03@gmail.com>
2022-08-28 22:52:14 -0300

    clase texthub

---
+++ RedSocialDeportiva/Server/Controllers/TextHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RedSocialDeportiva.Server.Controllers
+{
+    public class TextHub : Hub
+    {
+        public async Task Send(string text)
+        {
+            await Clients.All.SendAsync("ReceiveInformation", text);
+        }
+    }
+}
