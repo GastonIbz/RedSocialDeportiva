66c20b0cd2475a602150956dfd08f056eabcfd73
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2022-09-23 11:44:13 -0300

    Creo EstadisticasXid

---
+++ RedSocial.BD/Data/Entidades/EstadisticasXId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocial.BD.Data.Entidades
+{
+    public class EstadisticasXId : EntityBase
+    { 
+        public string timeWindow { get; set; } 
+
+        public string image { get; set; } 
+
+
+    }
+}
---
+++ RedSocial.BD/Migrations/20220923144348_EstadisticasXId.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedSocial.BD.Migrations
+{
+    public partial class EstadisticasXId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
