b00ad77fe6578f230c237b91fa04bebcf9a9a7a5
Gastonibanez <81576503+Gastonibanez0@users.noreply.github.com>
2022-09-26 01:31:28 -0300

    Hover y Postbox en Home

--- RedSocialDeportiva/Client/Program.cs
+++ RedSocialDeportiva/Client/Program.cs
@@ -26,13 +26,14 @@ global using RedSocialDeportiva.Client.Adapters;
 using RedSocialDeportiva.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazorStrap();
 builder.Services.AddAuthorizationCore(); 
 
 #region Servicios agregados
